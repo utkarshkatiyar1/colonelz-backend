398b6e22db904230617d338b73e04348066fdce8
Arvind Maurya <ar789maurya@gmail.com>
2024-07-29 11:09:47 +0530

    add login url in email template code

--- controllers/adminControllers/createuser.controllers/createuser.controller.js
+++ controllers/adminControllers/createuser.controllers/createuser.controller.js
@@ -93,7 +93,7 @@ export const createUser = async (req, res) => {
                                         <p><strong>Password:  </strong>${password}</p>
                                          <p><strong>Organisation Name:  </strong>${user.organization}</p>
                                         <p>Please click on the following link to login:</p>
-                                        <p><a href="https://colonelz-crm.stg.initz.run/sign-in?redirectUrl=/app/crm/dashboard">Login</a></p>
+                                        <p><a href="https://colonelz.test.initz.run/sign-in?redirectUrl=/app/crm/dashboard">Login</a></p>
                                         <p>Please use the above credentials to log in to our system.</p>
                                         <p>If you have any questions or need assistance, feel free to contact us.</p>
                                         <p>Best regards,<br>COLONELZ</p>
