a0aba3f07e4fa2f5def04645c21cef7e66b69dca
Arvind Maurya <ar789maurya@gmail.com>
2024-08-13 17:35:05 +0530

    remove access of jr executive and hr and executive assistance

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -65,136 +65,136 @@ export const checkAvailableUserIsAdmin = async(req,res,next) =>{
     {
       next();
     }
-    else if (user.role ==='Executive Assistant')
-      {
+    // else if (user.role ==='Executive Assistant')
+    //   {
       
-      try {
-        const data = await fileuploadModel.find({});
-        if (data.length > 0) {
-          let projectData = []
+    //   try {
+    //     const data = await fileuploadModel.find({});
+    //     if (data.length > 0) {
+    //       let projectData = []
           
-          await Promise.all(data.map(async (element) => {
-            if (element.project_id != null) {
-              const check_project = await projectModel.findOne({ project_id: element.project_id });
-              if (check_project) {
-                projectData.push({
-                  project_name: element.project_name,
-                  project_id: element.project_id,
-                  client_name: check_project.client[0].client_name,
-                  project_type: check_project.project_type,
-                  project_status: check_project.project_status,
-                  files: element.files
-                });
-              }
-            }
+    //       await Promise.all(data.map(async (element) => {
+    //         if (element.project_id != null) {
+    //           const check_project = await projectModel.findOne({ project_id: element.project_id });
+    //           if (check_project) {
+    //             projectData.push({
+    //               project_name: element.project_name,
+    //               project_id: element.project_id,
+    //               client_name: check_project.client[0].client_name,
+    //               project_type: check_project.project_type,
+    //               project_status: check_project.project_status,
+    //               files: element.files
+    //             });
+    //           }
+    //         }
 
-          }))
-          let execution = [];
-          let design = [];
-          let completed = [];
-          let archive = [];
-          let projects = [];
-          const project = await projectModel.find({}).sort({ createdAt: -1 });
-          for (let i = 0; i < project.length; i++) {
-            if (project[i].project_status == "executing") {
-              execution.push(project[i]);
-            }
-            if (project[i].project_status == "designing") {
-              design.push(project[i]);
-            }
-            if (project[i].project_status == "completed") {
-              completed.push(project[i]);
+    //       }))
+    //       let execution = [];
+    //       let design = [];
+    //       let completed = [];
+    //       let archive = [];
+    //       let projects = [];
+    //       const project = await projectModel.find({}).sort({ createdAt: -1 });
+    //       for (let i = 0; i < project.length; i++) {
+    //         if (project[i].project_status == "executing") {
+    //           execution.push(project[i]);
+    //         }
+    //         if (project[i].project_status == "designing") {
+    //           design.push(project[i]);
+    //         }
+    //         if (project[i].project_status == "completed") {
+    //           completed.push(project[i]);
               
-            }
-            projects.push({
-              project_name: project[i].project_name,
-              project_id: project[i].project_id,
-              client_name: project[i].client[0].client_name,
-              project_type: project[i].project_type,
-              project_status: project[i].project_status,
-              project_end_date: project[i].project_end_date,
-              project_start_date: project[i].project_start_date,
-              project_budget: project[i].project_budget,
-              project_description: project[i].project_description,
-              designer: project[i].designer,
-              client: project[i].client,
-              lead_id: project[i].lead_id,
-              mom: project[i].mom,
-              leadmanager: project[i].leadmanager,
-              visualizer: project[i].visualizer,
-              timeline_date: project[i].timeline_date,
-              project_location: project[i].project_location,
-              project_updated_by: project[i].project_updated_by,
-              quotation: project[i].quotation,
+    //         }
+    //         projects.push({
+    //           project_name: project[i].project_name,
+    //           project_id: project[i].project_id,
+    //           client_name: project[i].client[0].client_name,
+    //           project_type: project[i].project_type,
+    //           project_status: project[i].project_status,
+    //           project_end_date: project[i].project_end_date,
+    //           project_start_date: project[i].project_start_date,
+    //           project_budget: project[i].project_budget,
+    //           project_description: project[i].project_description,
+    //           designer: project[i].designer,
+    //           client: project[i].client,
+    //           lead_id: project[i].lead_id,
+    //           mom: project[i].mom,
+    //           leadmanager: project[i].leadmanager,
+    //           visualizer: project[i].visualizer,
+    //           timeline_date: project[i].timeline_date,
+    //           project_location: project[i].project_location,
+    //           project_updated_by: project[i].project_updated_by,
+    //           quotation: project[i].quotation,
 
-            })
-          }
+    //         })
+    //       }
         
         
 
          
 
-          const response = {
-            total_Project: projects.length,
-            Execution_Phase: execution.length,
-            Design_Phase: design.length,
-            completed: completed.length,
-            archive: archive.length,
-            active_Project: projects.length - completed.length,
-            projects,
-            projectData: projectData,
+    //       const response = {
+    //         total_Project: projects.length,
+    //         Execution_Phase: execution.length,
+    //         Design_Phase: design.length,
+    //         completed: completed.length,
+    //         archive: archive.length,
+    //         active_Project: projects.length - completed.length,
+    //         projects,
+    //         projectData: projectData,
           
-          }
-          responseData(
-            res,
-            `Data Found Successfully !`,
-            200,
-            true,
-            "",
-            response
-          );
-        }
-        if (data.length < 1) {
+    //       }
+    //       responseData(
+    //         res,
+    //         `Data Found Successfully !`,
+    //         200,
+    //         true,
+    //         "",
+    //         response
+    //       );
+    //     }
+    //     if (data.length < 1) {
 
-          responseData(
-            res,
-            "Data Not Found!",
-            200,
-            true,
-            " ",
+    //       responseData(
+    //         res,
+    //         "Data Not Found!",
+    //         200,
+    //         true,
+    //         " ",
 
-          );
-        }
-      } catch (err) {
-        res.send(err);
-        console.log(err);
-      }
+    //       );
+    //     }
+    //   } catch (err) {
+    //     res.send(err);
+    //     console.log(err);
+    //   }
 
 
-      }
-    else if (user.role ==='Jr. Executive HR & Marketing')
-    {
-      try{
-        let templateData = []
+    //   }
+    // else if (user.role ==='Jr. Executive HR & Marketing')
+    // {
+    //   try{
+    //     let templateData = []
 
-        const fileData = await fileuploadModel.find({ type:"template"})
-        if(fileData)
-        {
-          const template= fileData.find((item) => item.files.find((file) =>file.folder_name === "miscellaneous" && file.sub_folder_name_first === "miscellaneous"))
-          templateData.push(template)
-          const response ={
-            templateData
-          }
-          responseData(res, "user data found", 200, true, "", response)
-        }
+    //     const fileData = await fileuploadModel.find({ type:"template"})
+    //     if(fileData)
+    //     {
+    //       const template= fileData.find((item) => item.files.find((file) =>file.folder_name === "miscellaneous" && file.sub_folder_name_first === "miscellaneous"))
+    //       templateData.push(template)
+    //       const response ={
+    //         templateData
+    //       }
+    //       responseData(res, "user data found", 200, true, "", response)
+    //     }
 
-      }
-      catch(err)
-      {
-        return responseData(res, "", 401, false, "Unauthorized: Invalid token");
-      }
+    //   }
+    //   catch(err)
+    //   {
+    //     return responseData(res, "", 401, false, "Unauthorized: Invalid token");
+    //   }
 
-    }
+    // }
     else{
       cron.schedule(" 0 0 * * *", async () => {
         // This cron pattern runs the job at 00:00 every day
