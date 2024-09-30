36c232d472f3363884679f3548952b9230fbb947
Arvind Maurya <ar789maurya@gmail.com>
2024-09-30 11:09:24 +0530

    share contract (504) and delete file issue solve

--- controllers/adminControllers/fileUploadController/delete.file.controller.js
+++ controllers/adminControllers/fileUploadController/delete.file.controller.js
@@ -16,129 +16,57 @@ export const deleteFile = async (req, res) => {
 
     try {
         let count = 0;
-        for (let i = 0; i < fileIds.length; i++) {
-            let data;
-            let file;
 
-            if (type === "template") {
-                const filesData = await fileuploadModel.findOne(
-                    {
-                        "files.sub_folder_name_second": folder_name,
-                        "files.files.fileId": fileIds[i]
-                    },
+        for (const fileId of fileIds) {
+            const query = type === "template"
+                ? { "files.sub_folder_name_second": folder_name, "files.files.fileId": fileId }
+                : { $or: [{ project_id }, { lead_id }], "files.folder_name": folder_name, "files.files.fileId": fileId };
 
-                );
-                file = filesData.files.find(fileGroup => fileGroup.files.some(file => file.fileId === fileIds[i])).files.find(file => file.fileId === fileIds[i]);
+            const filesData = await fileuploadModel.findOne(query);
+            if (!filesData) continue;
 
-                data = await fileuploadModel.findOneAndUpdate(
-                    {
-                        "files.sub_folder_name_second": folder_name,
-                        "files.files.fileId": fileIds[i]
-                    },
-                    {
-                        $pull: {
-                            "files.$.files": { fileId: fileIds[i] }
-                        }
-                    },
-                    { new: true }
-                );
+            const fileGroup = filesData.files.find(group => group.files.some(file => file.fileId === fileId));
+            if (!fileGroup) continue;
 
-            } else {
-                const filesData = await fileuploadModel.findOne(
-                    {
-                        $or: [
-                            { project_id: project_id },
-                            { lead_id: lead_id },
-                        ],
-                        "files.folder_name": folder_name,
-                        "files.files.fileId": fileIds[i]
-                    },
+            const file = fileGroup.files.find(file => file.fileId === fileId);
+            const updateQuery = type === "template"
+                ? { "files.sub_folder_name_second": folder_name }
+                : { $or: [{ project_id }, { lead_id }], "files.folder_name": folder_name };
 
-                );
-               
-                file = filesData.files.find(fileGroup => fileGroup.files.some(file => file.fileId === fileIds[i])).files.find(file => file.fileId === fileIds[i]);
-
-                data = await fileuploadModel.findOneAndUpdate(
-                    {
-                        $or: [
-                            { project_id: project_id },
-                            { lead_id: lead_id },
-                        ],
-                        "files.folder_name": folder_name,
-                        "files.files.fileId": fileIds[i]
-                    },
-                    {
-                        $pull: {
-                            "files.$.files": { fileId: fileIds[i] }
-                        }
-                    },
-                    { new: true }
-                );
-            }
+            const data = await fileuploadModel.findOneAndUpdate(
+                updateQuery,
+                { $pull: { "files.$.files": { fileId } } },
+                { new: true }
+            );
 
             if (data) {
-                let name;
-                if (!lead_id && !type) {
-                    name = data.project_name;
-                   const check =  await Archive.create({
-                        lead_id,
-                        lead_name: type === "template" ? "" : "",
-                        project_name: type === "template" ? "" : name,
-                        project_id,
-                        folder_name,
-                        sub_folder_name_second: type === "template" ? folder_name : undefined,
-                        files: [file],
-                        type,
-                        deleted_type:"file"
-                    });
-                  
-                }
-                if (!project_id && !type) {
-                    name = data.lead_name;
-                   
-                    await Archive.create({
-                        lead_id,
-                        lead_name: type === "template" ? "" : name,
-                        project_name: type === "template" ? "" : "",
-                        project_id,
-                        folder_name,
-                        sub_folder_name_second: type === "template" ? folder_name : undefined,
-                        files: [file],
-                        type,
-                        deleted_type: "file"
-                    });
-                }
-                if (!lead_id && !project_id) {
-                    await Archive.create({
-                        lead_id,
-                        lead_name: type === "template" ? "" : "",
-                        project_name: type === "template" ? "" : "",
-                        project_id,
-                        folder_name: type === "template" ? data.files[0].folder_name : folder_name,
-                        sub_folder_name_first: type === "template" ? data.files[0].sub_folder_name_first : undefined,
-                        sub_folder_name_second: type === "template" ? data.files[0].sub_folder_name_second: undefined,
-                        files: [file],
-                        type,
-                        deleted_type: "file"
-                    });
-                }
+                const archiveData = {
+                    lead_id,
+                    lead_name: type === "template" ? "" : (lead_id ? data.lead_name : ""),
+                    project_name: type === "template" ? "" : (project_id ? data.project_name : ""),
+                    project_id,
+                    folder_name: type === "template" ? data.files[0]?.folder_name : folder_name,
+                    sub_folder_name_first: type === "template" ? data.files[0]?.sub_folder_name_first : undefined,
+                    sub_folder_name_second: type === "template" ? folder_name : undefined,
+                    files: [file],
+                    type,
+                    deleted_type: "file"
+                };
 
-
+                await Archive.create(archiveData);
                 count++;
             }
         }
 
-        if (count > 0) {
-            responseData(res, "Files moved to archive successfully", 200, true, "", []);
-        } else {
-            responseData(res, "No files found", 200, false, "", []);
-        }
+        const message = count > 0 ? "Files moved to archive successfully" : "No files found";
+        responseData(res, message, 200, count > 0, "", []);
 
     } catch (error) {
-        console.log(error);
+        console.error(error);
         responseData(res, "", 500, false, "Something went wrong", []);
     }
 };
+
 
 export const deleteFolder = async (req, res) => {
     const { lead_id, project_id, folder_name, type } = req.body;
@@ -148,120 +76,59 @@ export const deleteFolder = async (req, res) => {
     }
 
     try {
-        let count = 0;
-        let data;
-        let folder;
+        let folder, data;
+        const query = type === "template"
+            ? {
+                "files.folder_name": folder_name,
+                "files.sub_folder_name_first": req.body.sub_folder_name_first,
+                "files.sub_folder_name_second": req.body.sub_folder_name_second
+            }
+            : {
+                $or: [{ project_id }, { lead_id }],
+                "files.folder_name": folder_name
+            };
 
-        if (type === "template") {
-            const folder_name = req.body.folder_name;
-            const sub_folder_name_first = req.body.sub_folder_name_first;
-            const sub_folder_name_second = req.body.sub_folder_name_second;
-            const folderData = await fileuploadModel.findOne(
-                {
-                    "files.folder_name": folder_name,
-                    "files.sub_folder_name_first": sub_folder_name_first,
-                     "files.sub_folder_name_second": sub_folder_name_second },
-
-            );
-           
-            folder = folderData.files.find(fileGroup => fileGroup.folder_name === folder_name && fileGroup.sub_folder_name_first === sub_folder_name_first &&  fileGroup.sub_folder_name_second === sub_folder_name_second),
-
-
-                data = await fileuploadModel.findOneAndDelete(
-                    {
-                        "files.folder_name": folder_name,
-                        "files.sub_folder_name_first": sub_folder_name_first,
-                        "files.sub_folder_name_second": sub_folder_name_second
-},
-                    // { $pull: { "files": { sub_folder_name_second: sub_folder_name_second } } },
-                    // { new: true }
-                );
-
-        } 
-        else {
-            const folderData = await fileuploadModel.findOne(
-                {
-                    $or: [
-                        { project_id: project_id },
-                        { lead_id: lead_id },
-                    ],
-                    "files.folder_name": folder_name,
-                },
-            );
-
-            folder = folderData.files.find(fileGroup => fileGroup.folder_name === folder_name)
-            data = await fileuploadModel.findOneAndUpdate(
-                {
-                    $or: [
-                        { project_id: project_id },
-                        { lead_id: lead_id },
-                    ],
-                    "files.folder_name": folder_name,
-                },
-                { $pull: { "files": { folder_name: folder_name } } },
-                { new: true }
-            );
+        const folderData = await fileuploadModel.findOne(query);
+        if (!folderData) {
+            return responseData(res, "No folder found", 200, false, "", []);
         }
+
+        folder = folderData.files.find(fileGroup =>
+            type === "template"
+                ? fileGroup.folder_name === folder_name &&
+                fileGroup.sub_folder_name_first === req.body.sub_folder_name_first &&
+                fileGroup.sub_folder_name_second === req.body.sub_folder_name_second
+                : fileGroup.folder_name === folder_name
+        );
+
+        data = await fileuploadModel.findOneAndUpdate(
+            query,
+            { $pull: { "files": { folder_name } } },
+            { new: true }
+        );
 
         if (data) {
-            let name;
-            if (!lead_id && !type) {
-                name = data.project_name;
-                await Archive.create({
-                    lead_id,
-                    lead_name: type === "template" ? "" : "",
-                    project_name: type === "template" ? "" : name,
-                    project_id,
-                    folder_name: type === "template" ? folder.folder_name: folder_name ,
-                    sub_folder_name_first: type ==="template" ? folder.sub_folder_name_first : undefined,
-                    sub_folder_name_second: type === "template" ? folder.sub_folder_name_second : undefined,
-                    files: [folder],
-                    type,
-                    deleted_type: "folder"
-                    
-                });
-            }
-            if (!project_id && !type) {
-                name = data.lead_name;
-                await Archive.create({
-                    lead_id,
-                    lead_name: type === "template" ? "" : name,
-                    project_name: type === "template" ? "" : "",
-                    project_id,
-                    folder_name,
-                    sub_folder_name_second: type === "template" ? folder_name : undefined,
-                    files: [folder],
-                    type,
-                    deleted_type: "folder"
-                });
-            }
-            if (!lead_id && !project_id)
-                {
-                name = data.project_name;
-                await Archive.create({
-                    lead_id,
-                    lead_name: type === "template" ? "" : "",
-                    project_name: type === "template" ? "" : name,
-                    project_id,
-                    folder_name: type === "template" ? folder.folder_name : folder_name,
-                    sub_folder_name_first: type === "template" ? folder.sub_folder_name_first : undefined,
-                    sub_folder_name_second: type === "template" ? folder.sub_folder_name_second: undefined,
-                    files: [folder],
-                    type,
-                    deleted_type: "folder"
-                });
-                }
-            count++;
+            const archiveData = {
+                lead_id,
+                lead_name: type === "template" ? "" : (lead_id ? data.lead_name : ""),
+                project_name: type === "template" ? "" : (project_id ? data.project_name : ""),
+                project_id,
+                folder_name: type === "template" ? folder.folder_name : folder_name,
+                sub_folder_name_first: type === "template" ? folder.sub_folder_name_first : undefined,
+                sub_folder_name_second: type === "template" ? folder.sub_folder_name_second : undefined,
+                files: [folder],
+                type,
+                deleted_type: "folder"
+            };
+
+            await Archive.create(archiveData);
+            return responseData(res, "Folder moved to archive successfully", 200, true, "", []);
         }
 
-        if (count > 0) {
-            responseData(res, "Folder moved to archive successfully", 200, true, "", []);
-        } else {
-            responseData(res, "No folder found", 200, false, "", []);
-        }
-
+        responseData(res, "No folder found", 200, false, "", []);
     } catch (error) {
-        console.log(error);
+        console.error(error);
         responseData(res, "", 500, false, "Something went wrong", []);
     }
 };
+
